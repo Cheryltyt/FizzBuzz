11484eab0db35d19a5fc48a25c00936eecd6930e
Cheryltyt <cherylyanting@gmail.com>
2024-09-13 15:31:35 -0400

    Ticking checkboxes Adding Main Multiples of 1000

---
+++ src/Multiples.java
@@ -0,0 +1,40 @@
+public class Multiples {
+
+    public static void main(String[] args) {
+
+        int i = 1;
+        while (i < 1000) {
+
+            // Find out which numbers divide i.
+            extracted(i);
+            i++;
+        }
+    }
+
+    private static void extracted(int i) {
+        boolean divisibleBy3 = i % 3 == 0;
+        boolean divisibleBy5 = i % 5 == 0;
+
+        // Print our appropriate result.
+        if (divisibleBy3 && divisibleBy5) {
+
+            System.out.println("Fizz Buzz");
+
+        } else if (divisibleBy3) {
+
+            System.out.println("Fizz");
+
+        } else if (divisibleBy5) {
+
+            System.out.println("Buzz");
+
+        } else {
+
+            System.out.println(i);
+
+        }
+    }
+
+    public static void doFizzBuzz(int i) {
+    }
+}
